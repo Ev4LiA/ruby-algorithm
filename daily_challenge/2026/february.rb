403086bcddc3387d9f3df40e2db7f69ab70d78e2
Ev4LiA <ev4lia@users.noreply.github.com>
2026-02-04 00:44:55 +0700

    040226

--- daily_challenge/2026/february.rb
+++ daily_challenge/2026/february.rb
@@ -6,4 +6,26 @@ class February2026
     nums[1..-1] = nums[1..-1].sort!
     nums[0] + nums[1] + nums[2]
   end
+
+  # 3637. Tritonic Array I
+  # @param {Integer[]} nums
+  # @return {Boolean}
+  def is_trionic(nums)
+    p = 0
+    n = nums.length
+    p += 1 while p + 1 < n && nums[p] < nums[p + 1]
+    return false if p.zero? || p == n - 1
+
+    p += 1 while p + 1 < n && nums[p] > nums[p + 1]
+
+    return false if p >= n - 1
+
+    while p < n - 1
+      return false if nums[p + 1] <= nums[p]
+
+      p += 1
+    end
+
+    true
+  end
 end
