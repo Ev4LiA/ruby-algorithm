6cf075ca3192aff2c5517d03d6302e3579933c03
Ev4LiA <ev4lia@users.noreply.github.com>
2026-05-03 12:49:04 +0700

    030526

--- daily_challenge/2026/may.rb
+++ daily_challenge/2026/may.rb
@@ -9,7 +9,7 @@ class May2026
     (0...n).each { |i| f += i * nums[i] }
     max_f = f
     (1...n).each do |k|
-      f += sum - n * nums[n - k]
+      f += sum - (n * nums[n - k])
       max_f = [max_f, f].max
     end
     max_f
@@ -22,13 +22,20 @@ class May2026
     count = 0
     (1..n).each do |i|
       s = i.to_s
-      if s.include?('3') || s.include?('4') || s.include?('7')
-        next
-      end
-      if s.include?('2') || s.include?('5') || s.include?('6') || s.include?('9')
-        count += 1
-      end
+      next if s.include?("3") || s.include?("4") || s.include?("7")
+
+      count += 1 if s.include?("2") || s.include?("5") || s.include?("6") || s.include?("9")
     end
     count
+  end
+
+  # 796. Rotate String
+  # @param {String} s
+  # @param {String} goal
+  # @return {Boolean}
+  def rotate_string(s, goal)
+    return false if s.length != goal.length
+
+    (s + s).include?(goal)
   end
 end
